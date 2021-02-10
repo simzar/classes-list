61f71d3e042871f20b2c2977810a2681c6dc8957
Simonas Zaronskis <simonas@billo.app>
2021-02-10 20:07:08 +0200

    Configure react-intl, add all required translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FormattedMessage, IntlProvider } from 'react-intl';
+import messages from './lang/en.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <IntlProvider locale="en" messages={messages.translations}>
+                    <FormattedMessage tagName="h1" id="classesList.classes"/>
+                    <FormattedMessage tagName="h1" values={{ studentsCount: 1 }} id="studentsList.students"/>
+                    <FormattedMessage tagName="h1" values={{ studentsCount: 2 }} id="studentsList.students"/>
+                </IntlProvider>
+            </header>
+        </div>
+    );
 }
 
 export default App;
