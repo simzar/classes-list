257e8b08945109b8a5013d3988144e7eb538f660
Simonas Zaronskis <simonas@billo.app>
2021-02-13 15:36:11 +0200

    Use simple buttons over material-ui, improve keyboard navigation styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React, { MouseEventHandler } from 'react';
-import IconButton from '@material-ui/core/IconButton';
 import classes from './Button.module.scss';
 
 interface ButtonProps {
@@ -10,9 +9,9 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ onClick, className, Icon, alt }) => (
-    <IconButton onClick={onClick} classes={{ root: `${classes.root} ${className}` }}>
+    <button onClick={onClick} className={`${classes.root} ${className}`}>
         <Icon title={alt} />
-    </IconButton>
+    </button>
 );
 
 export default Button;
