3472ce106336339c48164cc9c8e7005f30babffe
Simonas Zaronskis <simonas@billo.app>
2021-02-15 23:45:32 +0200

    Style the 'move to' menu

--- src/components/SelectMenu/SelectMenu.tsx
+++ src/components/SelectMenu/SelectMenu.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
 import { MuiThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core';
+import classes from './SelectMenu.module.scss';
 
 export interface SelectMenuProps {
     title: string;
@@ -14,10 +16,22 @@ export interface SelectMenuProps {
 // TODO: all aria attributes, etc.
 const SelectMenu: React.FC<SelectMenuProps> = ({ title, items, handleSelect, anchorElement, handleClose }) => (
     <MuiThemeProvider theme={unstable_createMuiStrictModeTheme()}>
-        <Menu open={!!anchorElement} anchorEl={anchorElement} onClose={handleClose}>
-            {title}
+        <Menu
+            open={!!anchorElement}
+            anchorEl={anchorElement}
+            onClose={handleClose}
+            classes={{ paper: classes.root }}
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            style={{ transform: 'translate(25px, 35px)' }}
+            autoFocus
+        >
+            <div className={classes.title}>{title}</div>
             {items.map((item) => (
-                <MenuItem key={item} onClick={() => handleSelect(item)}>{item}</MenuItem>
+                <MenuItem classes={{ root: classes.listItem }} key={item} onClick={() => handleSelect(item)}>
+                    <Typography variant="inherit" noWrap>
+                        {item}
+                    </Typography>
+                </MenuItem>
             ))}
         </Menu>
     </MuiThemeProvider>
